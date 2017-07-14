782466a755605a1c690a47aafecb9b75aa327054
Olivia Frost <olivia.frost617@googlemail.com>
2017-07-14 11:05:34 +0100

    Session 2 Challenge 3 complete

---
+++ session2/challenge/3_array.rb
@@ -0,0 +1,16 @@
+# Write a method named every_other_char for strings that,
+# returns an array containing every other character
+#
+# example:
+# "abcdefg".every_other_char  # => "aceg"
+# "".every_other_char         # => ""
+
+class String
+  def every_other_char
+    new_string = ''
+    each_char.with_index do |char, index|
+      new_string << char if index.even?
+    end
+    new_string
+  end
+end
