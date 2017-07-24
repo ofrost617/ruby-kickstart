7d767b7881ea4b2bfc9604d005306e59f94b2968
Olivia Frost <olivia.frost617@googlemail.com>
2017-07-24 09:59:26 +0100

    Session 3 Challenges completed

---
+++ session3/challenge/8_blocks.rb
@@ -0,0 +1,42 @@
+# This problem is based off of example 2
+# Modify it so that it also tracks a person's age and a quote
+# Allow any of these to be set with a hash as well, but the block should overwrite the hash
+#
+#
+# EXAMPLE:
+#
+# artist = Person.new :name => 'Prince' do |person|
+#   person.age   = 47
+#   person.quote = "Why don't you purify yourself in the waters of Lake Minnetonka?"
+# end
+#
+# artist.name   # => "Prince"
+# artist.age    # => 47
+#
+# artist.name = 'The Artist Formarly Known As Prince'
+# artist.age  = 1999
+#
+# artist.name   # => "The Artist Formarly Known As Prince"
+# artist.age    # => 1999
+#
+# artist.reinit
+#
+# artist.name   # => "The Artist Formarly Known As Prince"
+# artist.age    # => 47
+
+
+class Person
+  attr_accessor :name, :age, :quote
+
+  def initialize(options=Hash.new, &initializer)
+    self.name = options[:name]
+    self.age = options[:age]
+    self.quote = options[:quote]
+    @initializer = (initializer || Proc.new { |person| })
+    reinit
+  end
+
+  def reinit
+    @initializer.call self
+  end
+end
